08be3ac59e2b14d60f3048a42a1d970be2936868
InigoFreire <inifreire@gmail.com>
2024-10-29 19:56:55 +0100

    Merge w/ Ander

--- src/dataAccessTier/DAO.java
+++ src/dataAccessTier/DAO.java
@@ -5,10 +5,143 @@
  */
 package dataAccessTier;
 
+<<<<<<< HEAD
 /**
  *
  * @author inifr
  */
 public class DAO {
     
+=======
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import userLogicTier.Signable;
+import userLogicTier.model.User;
+
+/**
+ *
+ * @author Ander
+ */
+public class DAO {
+
+    private Connection connection = null;
+
+    public User signUp(User user) throws SQLException {
+        boolean estado = false;
+        PreparedStatement stmtPartner = null;
+        PreparedStatement stmtUser = null;
+
+        connection = getConnection();
+
+        String insertPartner = "INSERT INTO res_partner(company_id, name, street, city, zip, email) VALUES "
+                + "(1, ?, ?, ?, ?, ?);";
+        String insertUser = "INSERT INTO res_users(company_id, parner_id, login, password, active, notification_type) VALUES "
+                + "(1, ?, ?, ?, ?, email);";
+
+        try {
+            // Le indicamos el inicio de la transacción
+            connection.setAutoCommit(false);
+
+            // Preparo el statement de partner para prevenir inyecciones maliciosas
+            stmtPartner = connection.prepareStatement(insertPartner, Statement.RETURN_GENERATED_KEYS);
+
+            // Le paso los datos
+            stmtPartner.setString(1, user.getName());
+            stmtPartner.setString(2, user.getStreet());
+            stmtPartner.setString(3, user.getCity());
+            stmtPartner.setString(4, user.getZip());
+            stmtPartner.setString(5, user.getEmail());
+
+            // Ejecuto la actualización de la base de datos
+            stmtPartner.executeUpdate();
+
+            // Obtener el ID generado
+            ResultSet generatedKeys = stmtPartner.getGeneratedKeys();
+            int partnerId = 0;
+            if (generatedKeys.next()) {
+                // Obtener el primer campo de la clave generada
+                partnerId = generatedKeys.getInt(1);
+            }
+
+            // Preparo el statement de users
+            stmtUser = connection.prepareStatement(insertUser);
+
+            // Le paso los datos
+            // El primero siempre 1
+            stmtUser.setInt(1, partnerId);
+            stmtUser.setString(2, user.getEmail());
+            stmtUser.setString(3, user.getPassword());
+            stmtUser.setBoolean(4, user.isActive());
+            // El último siempre email
+
+            // Ejecuto la actualización de la base de datos
+            stmtUser.executeUpdate();
+
+            // Si todo va bien hacemos commit y guardamos los datos
+            connection.commit();
+            estado = true;
+        } catch (SQLException e) {
+            // Si ocurre un error, realizar rollback
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            e.printStackTrace();
+            estado = false;
+        } finally {
+            // Cerrar recursos
+            if (stmtPartner != null) {
+                stmtPartner.close();
+            }
+            if (stmtUser != null) {
+                stmtUser.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return user;
+    }
+
+    public User signIn(User user) throws SQLException {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
+        String selectUserData = "SELECT login, password, active FROM res_users ";
+        String selectPartnerData = "SELECT name, street, city, zip FROM res_partner";
+
+        try {
+            stmt = connection.prepareStatement(selectUserData);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                user = new User();
+                user.setId(enunciadoId);
+                user.setDescripcion(rs.getString("descripcion"));
+                user.setNivel(Dificultad.valueOf(nivelStr));
+                user.setDisponible(rs.getBoolean("disponible"));
+                user.setRuta(rs.getString("ruta"));
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+        return user;
+    }
+
+    // accede a la conexión de la clase Pool
+    private Connection getConnection() throws SQLException {
+        return Pool.getConexion();
+    }
+>>>>>>> ander
 }
---
+++ src/dataAccessTier/Pool.java
@@ -0,0 +1,57 @@
+package dataAccessTier;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+/**
+ *
+ * @author Ander
+ */
+public class Pool {
+
+    private static BasicDataSource ds = null;
+    private static String username;
+    private static String password;
+    private static String url;
+
+    // Asigna el usuario y contraseña desde el archivo de propiedades para entrar a la base de datos
+    public static void getDatabaseCredentials() {
+        ResourceBundle configFile;
+        // Ruta de archivo de propiedades
+        configFile = ResourceBundle.getBundle("resources.Config");
+        username = configFile.getString("USERNAME");
+        password = configFile.getString("PASSWORD");
+        url = configFile.getString("URL");
+    }
+
+    //Este método crea y configura el pool de conexiones si aún no ha sido inicializado (ds == null)
+    public static DataSource getDataSource() {
+        if (ds == null) {
+            ds = new BasicDataSource();
+            // Define el controlador JDBC para PostgreSQL
+            ds.setDriverClassName("org.postgresql.Driver");
+            ds.setUsername(username);
+            ds.setPassword(password);
+            ds.setUrl(url);
+            // Número de conexiones iniciales en el pool cuando se crea
+            ds.setInitialSize(10);
+            // Número máximo de conexiones que pueden quedar ociosas en el pool sin ser cerradas (ociosas: abiertas sin actividad)
+            ds.setMaxIdle(10);
+            // Número total máximo de conexiones abiertas que puede manejar el pool
+            ds.setMaxTotal(20);
+            // Tiempo máximo en milisegundos que un hilo puede esperar para obtener una conexión antes de lanzar una excepción (-1 espera de manera ilimitada)
+            ds.setMaxWaitMillis(10000);
+        }
+        return ds;
+    }
+
+    public static Connection getConexion() throws SQLException {
+        // Devuelve una conexión del pool llamando al método getConnection() de DataSource
+        // La conexión que se devuelve es del tipo Connection, lista para ser usada en operaciones de base de datos
+        // Si no hay conexiones disponibles en el pool y se alcanza el tiempo de espera (MaxWaitMillis), lanzará una excepción SQLException
+        return getDataSource().getConnection();
+    }
+}
